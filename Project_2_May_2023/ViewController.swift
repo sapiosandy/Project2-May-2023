c7c4abb2e1e4c54fa31667f5691533e44cdf368b
Sandy Gomez <sgomez1029@gmail.com>
2023-05-02 12:19:45 -0700

    Initial Commit

---
+++ Project_2_May_2023/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project_2_May_2023
+//
+//  Created by Sandra Gomez on 5/2/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
